c87cb14bff69057a554ae3003b6a966b4b499c4a
MJoseHG <mariajosehgez@gmail.com>
2022-09-23 13:37:29 +0200

    Connect UserActivity to return MainActivity

--- app/src/main/java/com/example/myfirstapplication/ui/UserActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/ui/UserActivity.kt
@@ -2,7 +2,10 @@ package com.example.myfirstapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.myfirstapplication.R
+import com.google.android.material.appbar.MaterialToolbar
+
 
 class UserActivity : AppCompatActivity() {
 
@@ -11,9 +14,11 @@ class UserActivity : AppCompatActivity() {
         setContentView(R.layout.activity_users)
         supportActionBar?.hide()
 
-//        val arrowReturn =findViewById<>(R.id.topAppBarReturn)
-//        arrowReturn.setOnClickListener{
-//            finish()
-//        }
+        val topAppBarReturn = findViewById<MaterialToolbar>(R.id.topAppBarReturn) as Toolbar
+
+        topAppBarReturn.setNavigationOnClickListener {
+            finish()
+        }
+
     }
 }
